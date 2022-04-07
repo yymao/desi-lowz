325510e47b5a1ea2b2adde99e140564aa86a0684
Yao-Yuan Mao <yymao.astro@gmail.com>
2022-04-07 11:27:32 -0700

    add many-cutouts.py

---
+++ many-cutouts.py
@@ -0,0 +1,96 @@
+#!/usr/bin/python3
+
+"""
+many-cutouts.py
+---------------
+Modified by: Yao-Yuan Mao (yymao)
+Modified from: https://github.com/legacysurvey/imagine/blob/master/many-cutouts.py
+Original author: Dustin Lang (dstndstn)
+
+Usage on NERSC
+--------------
+shifter --image=dstndstn/viewer-cutouts:latest /bin/bash
+/usr/bin/python3 many-cutouts.py list_of_objects.fits --outdir=output_dir_path [OTHER OPTIONS]
+"""
+
+import sys
+
+# https://github.com/legacysurvey/imagine/blob/master/docker/cutout#L4
+sys.path = [
+  "/usr/lib/python3.8",
+  "/usr/lib/python3.8/lib-dynload",
+  "/usr/local/lib/python3.8/dist-packages",
+  "/app/src/astrometry",
+  "/app/src/tractor",
+  "/app/src/legacypipe/build/lib",
+  "/usr/lib/python38.zip",
+  "/usr/lib/python3/dist-packages",
+  "/app/imagine",
+]
+
+import os
+import argparse
+
+import numpy as np
+from astropy.table import Table
+
+from map.views import get_layer, NoOverlapError
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('table', help='Table of cutouts to produce')
+    parser.add_argument('--outdir', default=os.curdir, help='Directory prefix to add to output filenames')
+    parser.add_argument('--pixscale', type=float, default=0.262, help='Pixel scale (arcsec/pix)')
+    parser.add_argument('--size', type=int, default=224, help='Pixel size of output')
+    parser.add_argument('--width', type=int, default=None, help='Pixel width of output')
+    parser.add_argument('--height', type=int, default=None, help='Pixel height of output')
+    parser.add_argument('--bands', default='grz', help='Bands to select for output')
+    parser.add_argument('--layer', default='ls-dr9', help='Map layer to render')
+    parser.add_argument('--force', default=False, help='Overwrite existing output file?  Default is to quit.')
+    opt = parser.parse_args()
+
+    outdir = opt.outdir
+    pixscale = opt.pixscale
+    H = W = opt.size
+    if opt.height is not None:
+        H = opt.height
+    if opt.width is not None:
+        W = opt.width
+    bands = opt.bands
+    layer = get_layer(opt.layer)
+    os.makedirs(outdir, exist_ok=True)
+
+    T = Table.read(opt.table)
+    if "out" not in T.colnames:
+        if 'OBJID' in T.colnames:
+            T["out"] = np.char.mod("%d.jpg", T["OBJID"])
+        else:
+            T["out"] = np.char.add(np.char.mod("%.7f_", T["RA"]), np.char.mod("%.7f.jpg", T["DEC"]))
+
+    for t in T:
+        out = os.path.join(outdir, t['out'])
+        ra = t['RA']
+        dec = t['DEC']
+
+        if os.path.exists(out) and not opt.force:
+            print('Exists:', out)
+            continue
+
+        tempfiles = []
+        try:
+            layer.write_cutout(ra, dec, pixscale, W, H, out,
+                               bands=bands, fits=False, jpeg=True, tempfiles=tempfiles)
+        except NoOverlapError:
+            print('No overlap with {} ({}, {})'.format(out, ra, dec))
+        except Exception as e:
+            print('Error for {} ({}, {}): {}'.format(out, ra, dec, e))
+        finally:
+            for fn in tempfiles:
+                try:
+                    os.unlink(fn)
+                except:
+                    pass
+
+if __name__ == '__main__':
+    main()
+
